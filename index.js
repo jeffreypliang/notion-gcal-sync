d0b983909ef4773068335f1f269079dc3cb8778f
Jeffrey Liang <jeffrey.liang123@gmail.com>
2021-08-28 01:04:23 -0700

    start change from gtasks to gcal

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+/**
+ * Syncs Notion database to Google Calendar.
+ * 
+ * @author Jeffrey Liang
+ */
+
 const { Client } = require("@notionhq/client");
-const { google, tasks_v1 } = require("googleapis");
+const { google } = require("googleapis");
 const dotenv = require("dotenv");
 
 dotenv.config();
@@ -10,32 +16,52 @@ const googleClientId = process.env.GOOGLE_CLIENT_ID;
 const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET;
 const googleRefreshToken = process.env.GOOGLE_REFRESH_TOKEN;
 const googleRedirectUrl = "https://developers.google.com/oauthplayground";
-const googleTaskListTitle = process.env.GOOGLE_TASK_LIST_TITLE;
+const googleCalendarId = process.env.GOOGLE_CALENDAR_ID;
+
+const pageFilter = {
+    or: [
+        {
+            property: "Type",
+            select: {
+                equals: "Lab"
+            }
+        },
+        {
+            property: "Type",
+            select: {
+                equals: "Quiz"
+            }
+        },
+        {
+            property: "Type",
+            select: {
+                equals: "Homework"
+            }
+        },
+        {
+            property: "Type",
+            select: {
+                equals: "Project"
+            }
+        },
+        {
+            property: "Type",
+            select: {
+                equals: "Extra Credit"
+            }
+        },
+    ]
+}
 
 const googleAuth = createGoogleAuth();
-const tasksClient = google.tasks({
-    version: 'v1',
+const calendarClient = google.calendar({
+    version: 'v3',
     auth: googleAuth,
 });
 const notionClient = new Client({ auth: notionToken });
 
-async function printNotion() {
-    console.log(await getPages());
-}
-
-addTasks();
-printTasks();
-
-async function printPages() {
-    const pages = await getPages();
-    console.log(pages);
-}
-
-async function printTasks() {
-    const tasks = await getTasks();
-    console.log(tasks);
-}
-
+// setInterval(sync, 30000);
+// await sync();
 
 /**
  * Creates an authorized OAuth2 client using the provided credentials.
@@ -52,6 +78,19 @@ function createGoogleAuth() {
     return auth;
 }
 
+/**
+ * Syncs the given Notion database to the given Google Calendar.
+ */
+async function sync() {
+    const pages = await getPages();
+    syncCalendar(pages);
+}
+
+/**
+ * Returns a map of all pages in the specified database.
+ * 
+ * @returns {Map} A map, with page ids as keys and reduced pages as values.
+ */
 async function getPages() {
     const pages = [];
     let cursor = undefined;
@@ -59,45 +98,92 @@ async function getPages() {
         const { results, next_cursor } = await notionClient.databases.query({
             database_id: notionDatabaseId,
             start_cursor: cursor,
-            filter: {
-                or: [
-                    {
-                        property: "Type",
-                        select: {
-                            equals: "Lab"
-                        }
-                    },
-                    {
-                        property: "Type",
-                        select: {
-                            equals: "Quiz"
-                        }
-                    },
-                    {
-                        property: "Type",
-                        select: {
-                            equals: "Homework"
-                        }
-                    },
-                    {
-                        property: "Type",
-                        select: {
-                            equals: "Project"
-                        }
-                    },
-                    {
-                        property: "Type",
-                        select: {
-                            equals: "Extra Credit"
-                        }
-                    },
-                ]
-            },
+            filter: pageFilter,
         });
         pages.push(...results);
         cursor = next_cursor;
     } while (cursor);
-    return new Map(pages.map(page => {
+    return new Map(pages.map(page => [page.id, reducePage(page)]));
+}
+
+async function syncCalendar(pages) {
+    return;
+}
+
+/**
+ * Reduces a page object to only its necessary properties.
+ * 
+ * @param {Object} page A page object.
+ * @returns {Object} An object containing the pages id, name, course, date, and status.
+ */
+function reducePage(page) {
+    let name = undefined;
+    let course = undefined;
+    let date = undefined;
+    let status = undefined;
+    if (page.properties["Name"].title[0]) {
+        name = page.properties["Name"].title[0].plain_text;
+    }
+    if (page.properties["Course"].select) {
+        course = page.properties["Course"].select.name;
+    }
+    if (page.properties["Date"].date) {
+        if (page.properties["Date"].date.end) {
+            date = formatDateRFC(page.properties["Date"].date.end);
+        } else {
+            date = formatDateRFC(page.properties["Date"].date.start);
+        }
+    }
+    if (page.properties["Status"].select) {
+        status = page.properties["Status"].select.name;
+    }
+    return {
+        id: page.id,
+        name: name,
+        course: course,
+        date: date,
+        status: status,
+    };
+}
+
+async function addTasks() {
+    const taskList = await getTaskList();
+    const pages = await getPages();
+    for (const [key, value] of pages) {
+        let title = "";
+        let status = "needsAction";
+        let due = value.date;
+        if (value.name) {
+            if (value.course) {
+                title = `[${value.course}] ${value.name}`;
+            } else {
+                title = value.name;
+            }
+        }
+        if (value.status == "Done") {
+            status = "completed";
+        }
+        calendarClient.tasks.insert({
+            tasklist: taskList,
+            requestBody: {
+                title: title,
+                notes: key,
+                status: status,
+                due: due,
+            }
+        });
+    }
+}
+
+async function updateTasks() {
+    const taskList = await getTaskList();
+    const tasks = await getTasks();
+    for (const task of tasks) {
+        pageId = task.notes;
+        const page = await notionClient.pages.retrieve({
+            page_id: pageId,
+        });
+        console.log(page);
         let name = undefined;
         let course = undefined;
         let date = undefined;
@@ -118,57 +204,15 @@ async function getPages() {
         if (page.properties["Status"].select) {
             status = page.properties["Status"].select.name;
         }
-        return [page.id, {
-            name: name,
-            course: course,
-            date: date,
-            status: status,
-        }]
-    }));
-}
-
-async function addTasks() {
-    const taskList = await getTaskList();
-    const pages = await getPages();
-    for (const [key, value] of pages) {
-        let title = "";
-        let status = "needsAction";
-        let due = value.date;
-        if (value.name) {
-            if (value.course) {
-                title = `[${value.course}] ${value.name}`;
-            } else {
-                title = value.name;
-            }
-        }
-        if (value.status == "Done") {
-            status = "completed";
-        }
-        console.log(due);
-        tasksClient.tasks.insert({
-            tasklist: taskList,
-            requestBody: {
-                title: title,
-                notes: key,
-                status: status,
-                due: value.date,
-            }
-        });
-    }
-}
-
-async function updateTasks() {
-    const taskList = await getTaskList();
-    const tasks = await getTasks();
-    for (const task of tasks) {
-
-        tasksClient.tasks.update({
+        calendarClient.tasks.update({
             tasklist: taskList,
             task: task.id,
             requestBody: {
-                kind: "tasks#task",
                 id: task.id,
-                status: "needsAction",
+                title: name,
+                notes: pageId,
+                status: status,
+                due: date,
             }
         })
     }
@@ -179,7 +223,7 @@ async function getTasks() {
     const taskList = await getTaskList();
     let pageToken = undefined;
     do {
-        const { data } = await tasksClient.tasks.list({
+        const { data } = await calendarClient.calendarList.list({
             tasklist: taskList,
             pageToken: pageToken,
             showCompleted: true,
@@ -197,7 +241,7 @@ async function getTasks() {
 async function getTaskList() {
     let pageToken = undefined;
     do {
-        const { data } = await tasksClient.tasklists.list({
+        const { data } = await calendarClient.calendarList.list({
             pageToken: pageToken,
         });
         for (const list of data.items) {
